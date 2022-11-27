56791b3e61373c80fbb55277b13301e7b980f474
Dheeraj Prajapati <88883506+dprajapati18@users.noreply.github.com>
2022-11-27 14:13:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            body{
+                background-color:lightblue;
+                
+            }
+        </style>
+    </head>    
+<body>
+
+<form action="patel.php" method="post" enctype="multipart/form-data">
+  <b><u>Select image to upload:</u></b>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
